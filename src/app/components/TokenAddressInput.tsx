c7d516e2740c7dc471a764bea1e7f1728d920c40
et-taousy <zouhirtaousy@gmail.com>
2024-08-10 01:43:41 +0200

    feat: init sc call

--- src/app/components/TokenAddressInput.tsx
+++ src/app/components/TokenAddressInput.tsx
@@ -1,26 +1,47 @@
 'use client';
 
 import { Textarea, IconButton } from "@material-tailwind/react";
+import { useState } from "react";
+import { useReadContract, useReadContracts } from "wagmi";
+import { abi } from "../abis/erc20";
+import { Address } from "viem";
  
 export function TokenAddressInput() {
+   const [address, setAddress] = useState<Address>();
+   const contractConfig = {
+    abi,
+    address,
+   }
+   const { 
+    data,
+    error,
+    isPending
+  } = useReadContracts({ 
+    contracts: [{ 
+    
+      functionName: 'balanceOf',
+     ...contractConfig,
+      args: ['0x03A71968491d55603FFe1b11A9e23eF013f75bCF'],
+    }, { 
+      functionName: 'symbol',
+      ...contractConfig,
+    },
+    { 
+      functionName: 'decimals', 
+      ...contractConfig,
+    }, { 
+      functionName: 'totalSupply', 
+      ...contractConfig,
+    }] 
+  }) 
+  const [balance, symbol, decimals] = data || [] 
+
+ console.log(data);
   return (
     <div className="flex w-full flex-row items-center gap-2 rounded-[99px] border border-gray-900/10 bg-gray-900/5 p-2">
       <div className="flex">
         <IconButton variant="text" className="rounded-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="h-5 w-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-            />
-          </svg>
+         {symbol?.result}
         </IconButton>
         <IconButton variant="text" className="rounded-full">
           <svg
@@ -42,6 +63,8 @@ export function TokenAddressInput() {
       <Textarea
         rows={1}
         resize={true}
+        value={address}
+        onChange={e=>setAddress(e.target.value)}
         placeholder="Your Token Address"
         className="min-h-full !border-0 focus:border-transparent"
         containerProps={{
@@ -53,20 +76,7 @@ export function TokenAddressInput() {
       />
       <div>
         <IconButton variant="text" className="rounded-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-            className="h-5 w-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-            />
-          </svg>
+          {decimals?.result}
         </IconButton>
       </div>
     </div>
